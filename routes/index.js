7d5c8d9dbcc2bd70d72a96dfd18cf64b7be43d13
leonprou <leonp@colu.com>
2018-05-08 16:10:48 +0300

    add is_running

--- routes/index.js
+++ routes/index.js
@@ -2,4 +2,8 @@ var router = require('express').Router()
 
 router.use('/api/v1', require('./api'))
 
+router.get('/is_running', (req, res, next) => {
+  res.send({response: 'ok'})
+})
+
 module.exports = router
